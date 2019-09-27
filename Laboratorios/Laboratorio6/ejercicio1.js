552324bda3c8b537ffed453c52e006746f01b6e4
= <=>
2019-09-27 17:20:50 -0600

    ejercicio 1

--- Laboratorios/Laboratorio6/ejercicio1.js
+++ Laboratorios/Laboratorio6/ejercicio1.js
@@ -1,1 +1,15 @@
- 
+var funcion = x => {
+       let typeArr = [];
+       let counts = {};
+
+       x.forEach(element => {
+           typeArr.push(typeof element);
+       });
+
+       typeArr.forEach(element => {
+           counts[element] = (counts[element] || 0) +1;
+       });
+
+       console.log(typeArr);
+       console.log(counts);
+   }
